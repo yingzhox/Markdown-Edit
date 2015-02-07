c76bf1d5cb7d44441b8d2fb1bd31eabdb925f5bd
mike-ward <mike@mike-ward.net>
2015-02-07 16:48:44 -0500

    Something working. Gonna be a long slog.

--- src/MarkdownEdit/Models/MarkdownHighlightingColorizer.cs
+++ src/MarkdownEdit/Models/MarkdownHighlightingColorizer.cs
@@ -25,36 +25,70 @@ namespace MarkdownEdit.Models
 
             var start = line.Offset;
             var end = line.EndOffset;
+            var length = line.Length;
 
-            var blocks = ast
+            var astBlocks = ast
                 .AsEnumerable()
+                .Where(sb => sb.Block != null)
                 .Where(sb => sb.IsOpening)
-                .Where(sb => sb.Block != null && sb.Block.SourcePosition >= start)
-                .Where(sb => sb.Block != null && sb.Block.SourcePosition <= end);
-
-            foreach (var block in blocks)
-            {
-                if (block.Block.Tag == BlockTag.AtxHeader)
+                .Select(sb => new
                 {
-                    ChangeLinePart(block.Block.InlineContent.SourcePosition, block.Block.InlineContent.SourcePosition + block.Block.InlineContent.SourceLength,
-                        element => ApplyHighlight(element, theme.HighlightHeading));
+                    Block = sb,
+                    SourcePosition = sb.Block.InlineContent?.SourcePosition ?? sb.Block.SourcePosition,
+                    SourceLength = sb.Block.InlineContent?.SourceLength ?? sb.Block.SourceLength
+                })
+                .Where(a => a.SourcePosition <= end)
+                .Where(a => a.SourcePosition + a.SourceLength > start);
+
+            foreach (var astBlock in astBlocks)
+            {
+                switch (astBlock.Block.Block.Tag)
+                {
+                    case BlockTag.AtxHeader:
+                    case BlockTag.SETextHeader:
+                        ApplyLinePart(theme.HighlightHeading, astBlock.SourcePosition, astBlock.SourceLength, start, end, length);
+                        break;
+
+                    case BlockTag.BlockQuote:
+                        ApplyLinePart(theme.HighlightBlockQuote, astBlock.SourcePosition, astBlock.SourceLength, start, end, length);
+                        break;
+
+                    case BlockTag.ListItem:
+                        ApplyLinePart(theme.HighlightStrongEmphasis, astBlock.SourcePosition, astBlock.SourceLength, start, end, 1);
+                        break;
+
+                    case BlockTag.FencedCode:
+                    case BlockTag.IndentedCode:
+                        ApplyLinePart(theme.HighlightBlockCode, astBlock.SourcePosition, astBlock.SourceLength, start, end, length);
+                        break;
                 }
             }
+        }
+
+        private void ApplyLinePart(Highlight highlight, int sourceStart, int sourceLength, int lineStart, int lineEnd, int maxLength)
+        {
+            var start = Math.Max(sourceStart, lineStart);
+            var end = Math.Min(lineEnd, start + Math.Min(sourceLength, maxLength));
+            ChangeLinePart(start, end, element => ApplyHighlight(element, highlight));
         }
 
         public static void ApplyHighlight(VisualLineElement element, Highlight highlight)
         {
             var trp = element.TextRunProperties;
-            var tf = element.TextRunProperties.Typeface;
 
             var foregroundBrush = ColorBrush(highlight.Foreground);
             if (foregroundBrush != null) trp.SetForegroundBrush(foregroundBrush);
 
-            trp.SetFontRenderingEmSize(trp.FontRenderingEmSize * 1.2);
-            var weight = ConvertFontWeight(highlight.FontWeight, tf.Weight);
-            var style = ConvertFontStyle(highlight.FontStyle, tf.Style);
+            var backgroundBrush = ColorBrush(highlight.Background);
+            if (backgroundBrush != null) trp.SetForegroundBrush(backgroundBrush);
+
+            var tf = element.TextRunProperties.Typeface;
+            var weight = ConvertFontWeight(highlight.FontWeight) ?? tf.Weight;
+            var style = ConvertFontStyle(highlight.FontStyle) ?? tf.Style;
             var typeFace = new Typeface(tf.FontFamily, style, weight, tf.Stretch);
             trp.SetTypeface(typeFace);
+
+            if (highlight.Underline) trp.SetTextDecorations(TextDecorations.Underline);
         }
 
         private static Brush ColorBrush(string color)
@@ -70,29 +104,29 @@ namespace MarkdownEdit.Models
             }
         }
 
-        private static FontWeight ConvertFontWeight(string fontWeight, FontWeight defaultWeight)
+        private static FontWeight? ConvertFontWeight(string fontWeight)
         {
-            if (string.IsNullOrWhiteSpace(fontWeight)) return defaultWeight;
+            if (string.IsNullOrWhiteSpace(fontWeight)) return null;
             try
             {
                 return (FontWeight)new FontWeightConverter().ConvertFromString(fontWeight);
             }
             catch (NotSupportedException)
             {
-                return defaultWeight;
+                return null;
             }
         }
 
-        private static FontStyle ConvertFontStyle(string fontStyle, FontStyle defaultStyle)
+        private static FontStyle? ConvertFontStyle(string fontStyle)
         {
-            if (string.IsNullOrWhiteSpace(fontStyle)) return defaultStyle;
+            if (string.IsNullOrWhiteSpace(fontStyle)) return null;
             try
             {
                 return (FontStyle)(new FontStyleConverter().ConvertFromString(fontStyle));
             }
             catch (NotSupportedException)
             {
-                return defaultStyle;
+                return null;
             }
         }
 
